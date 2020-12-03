18e6a78c1e288832ea76fe356da2ff5f5c3c3a9c
gyz0807-ai <gyz.ai.dev@gmail.com>
2020-12-02 20:51:59 -0800

    Added models and live streaming

--- src/utils/nn.py
+++ src/utils/nn.py
@@ -20,8 +20,11 @@ def nn_base(input_img_size):
     out = keras.layers.Conv2D(32, [3, 3], [2, 2], 'same')(out)
     out = keras.layers.Conv2D(64, [3, 3], [2, 2], 'same')(out)
     out = keras.layers.Conv2D(128, [3, 3], [2, 2], 'same')(out)
-    out = keras.layers.GlobalAvgPool2D()(out)
+    # out = keras.layers.GlobalAvgPool2D()(out)
+    out = keras.layers.Flatten()(out)
+    out = keras.layers.Dense(1000)(out)
     out = keras.layers.Dense(30)(out)
+    out = keras.activations.sigmoid(out)
     return keras.Model(inputs=tf_input, outputs=out)
 
 def nn_cbl(input_img_size, is_training=False):
@@ -32,4 +35,34 @@ def nn_cbl(input_img_size, is_training=False):
     out = cbl(out, 128, [3, 3], [2, 2], is_training)
     out = keras.layers.GlobalAvgPool2D()(out)
     out = keras.layers.Dense(30)(out)
+    out = keras.activations.sigmoid(out)
+    return keras.Model(inputs=tf_input, outputs=out)
+
+def nn_new(input_img_size, is_training=False):
+    tf_input = keras.Input(input_img_size)
+    out = keras.layers.Conv2D(256, [3, 3], 2, activation='relu')(tf_input)
+    out = keras.layers.Conv2D(256, [3, 3], 2, activation='relu')(out)
+    out = keras.layers.BatchNormalization()(out)
+
+    out = keras.layers.Conv2D(128, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.Conv2D(128, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.BatchNormalization()(out)
+
+    out = keras.layers.Conv2D(128, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.Conv2D(128, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.BatchNormalization()(out)
+
+    out = keras.layers.Conv2D(64, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.Conv2D(64, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.BatchNormalization()(out)
+
+    out = keras.layers.Conv2D(32, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.Conv2D(32, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.BatchNormalization()(out)
+
+    out = keras.layers.Conv2D(30, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.Conv2D(30, [3, 3], 1, activation='relu')(out)
+    out = keras.layers.Conv2D(30, [3, 3], 1)(out)
+    out = keras.layers.Flatten()(out)
+
     return keras.Model(inputs=tf_input, outputs=out)
